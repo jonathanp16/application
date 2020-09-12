3251ccd19a82223eeb5a57e63ca4f9b734187852
Andrew H <andrew@plankdesign.com>
2020-09-11 21:51:57 -0400

    fix(#26): Setup useful composer scripts and a few standard laravel debug packages

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        if (class_exists(\Laravel\Telescope\Telescope::class)) {
+            $schedule->command('telescope:prune')->daily();
+        }
     }
 
     /**
