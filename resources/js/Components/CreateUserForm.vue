468719f1402fd71c506e0ce9bd3c126ef5aed10e
Evan Dimopoulos <evan.dime@gmail.com>
2020-10-06 11:26:48 -0400

    feat(#42): Initial commit for creating other users.
    The vue does not show or update when i run npm commands.
    Will need to be updated once we get roles so that only Admins can see this form

---
+++ resources/js/Components/CreateUserForm.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <form class="w-full max-w-lg">
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="password" type="password" placeholder="***********">
+                <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password_confirmation">
+                    Password Confirmation
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="password_confirmation" type="password" placeholder="***********">
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+                    Name
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="name" type="text" placeholder="John Doe">
+                <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+            </div>
+        </div>
+    </form>
+</template>
+<script>
+
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -22,6 +22,7 @@
 
     export default {
         components: {
+            CreateUserForm,
             AppLayout,
             Welcome,
         },
