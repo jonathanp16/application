7d539c98930d55d6f5219af54cea691db1e8799b
Evan Dimopoulos <evan.dime@gmail.com>
2020-10-18 15:53:54 -0400

    chore(#42): cleaning up unused routes
    see https://github.com/CSU-Booking-Platform/application/pull/126#discussion_r506981673

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)->only(['create', 'store']);
 
 Route::get('/', function () {
     return view('welcome');
