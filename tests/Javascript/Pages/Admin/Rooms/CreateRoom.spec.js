57095ea350afee750115a04568c3f42b0d51c1a6
Joey Abou-Chaker <joeyta_ac@hotmail.com>
2020-11-21 14:55:57 -0500

    fix(#53): wrongs imports fixed

--- tests/Javascript/Pages/Admin/Rooms/CreateRoom.spec.js
+++ tests/Javascript/Pages/Admin/Rooms/CreateRoom.spec.js
@@ -5,8 +5,8 @@ import {createLocalVue, mount} from '@vue/test-utils'
 import {InertiaApp} from "@inertiajs/inertia-vue";
 import {InertiaForm} from "laravel-jetstream";
 import {InertiaFormMock} from "@test/__mocks__/laravel-jetstream";
-import CreateRoomForm from "@src/Pages/Admin/rooms/CreateRoomForm";
-import Index from "@src/Pages/Admin/rooms/Index";
+import CreateRoomForm from "@src/Pages/Admin/Rooms/CreateRoomForm";
+import Index from "@src/Pages/Admin/Rooms/Index";
 
 let localVue;
 
