cc83e2cf7955b16274e31ab2e13481fc47c4650a
Jonathan Perez <perezjonathan301@gmail.com>
2020-12-18 18:06:45 -0500

    fix(#39): Added booking request tests

---
+++ tests/Javascript/Pages/Admin/BookingRequests/CreateBookingRequest.spec.js
@@ -0,0 +1,70 @@
+import {afterEach, beforeEach, jest, test} from "@jest/globals";
+
+jest.mock('laravel-jetstream')
+import {createLocalVue, mount} from '@vue/test-utils'
+import {InertiaApp} from "@inertiajs/inertia-vue";
+import {InertiaForm} from "laravel-jetstream";
+import {InertiaFormMock} from "@test/__mocks__/laravel-jetstream";
+import CreateBookingRequestForm from "@src/Pages/Admin/BookingRequests/CreateBookingRequestForm";
+import Index from "@src/Pages/Admin/Rooms/Index";
+
+let localVue;
+
+
+beforeEach(() => {
+    InertiaFormMock.error.mockClear()
+    InertiaFormMock.post.mockClear()
+
+    localVue = createLocalVue()
+    localVue.use(InertiaApp)
+    localVue.use(InertiaForm)
+
+});
+afterEach(() => {
+    localVue = null
+})
+
+test('should mount without crashing', () => {
+
+    const wrapper = mount(CreateBookingRequestForm, {localVue})
+
+    expect(wrapper.text()).toBeDefined()
+})
+
+
+test('createBookingRequest when no form errors', () => {
+
+    InertiaFormMock.post.mockReturnValueOnce({
+        then(callback) {
+            callback({})
+        }
+    })
+
+    InertiaFormMock.hasErrors.mockReturnValueOnce(false)
+
+    const wrapper = mount(CreateBookingRequestForm, {localVue})
+
+    wrapper.vm.createBookingRequest()
+
+    expect(InertiaFormMock.post).toBeCalledTimes(1)
+
+})
+
+test('createBookingRequest when form errors', () => {
+
+    InertiaFormMock.post.mockReturnValueOnce({
+        then(callback) {
+            callback({})
+        }
+    })
+
+    InertiaFormMock.hasErrors.mockReturnValueOnce(true)
+    InertiaFormMock.error.mockReturnValueOnce("Some name error")
+
+    const wrapper = mount(CreateBookingRequestForm, {localVue})
+
+    wrapper.vm.createBookingRequest()
+
+    expect(InertiaFormMock.post).toBeCalledTimes(1)
+
+})
---
+++ tests/Javascript/Pages/Admin/BookingRequests/Index.spec.js
@@ -0,0 +1,28 @@
+import {beforeEach, jest, test} from "@jest/globals";
+
+jest.mock('laravel-jetstream')
+
+import {createLocalVue, mount, shallowMount} from '@vue/test-utils'
+import {InertiaApp} from '@inertiajs/inertia-vue'
+import {InertiaForm} from 'laravel-jetstream'
+import Index from '@src/Pages/Admin/BookingRequests/Index'
+import {InertiaFormMock} from "@test/__mocks__/laravel-jetstream";
+import moment from "moment";
+
+let localVue
+
+beforeEach(() => {
+    InertiaFormMock.error.mockClear()
+    InertiaFormMock.post.mockClear()
+    InertiaFormMock.delete.mockClear()
+
+    localVue = createLocalVue()
+    localVue.use(InertiaApp)
+    localVue.use(InertiaForm)
+
+});
+
+test('should mount without crashing', () => {
+    const wrapper = shallowMount(Index, {localVue})
+})
+
