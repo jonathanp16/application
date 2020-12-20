5ac907a04a1a688c1494555d0544cff3ee78377c
Joey Abou-Chaker <joeyta_ac@hotmail.com>
2020-12-20 18:15:08 -0500

    fix(#212) Navbar Component

--- tests/Javascript/Components/Navbar.spec.js
+++ tests/Javascript/Components/Navbar.spec.js
@@ -5,7 +5,7 @@ import {createLocalVue, mount} from '@vue/test-utils'
 import {InertiaApp} from "@inertiajs/inertia-vue";
 import {InertiaForm} from "laravel-jetstream";
 import {InertiaFormMock} from "@test/__mocks__/laravel-jetstream";
-import Navbar from "@src/Components/Navbar/NavBar";
+import Navbar from "@src/Components/Navbar/Navbar";
 
 
 let localVue;
