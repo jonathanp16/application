c28124279a92208a3efd7c4c5b34744007251e89
Jonathan Perez <perezjonathan301@gmail.com>
2020-12-20 22:40:29 -0500

    fix(#39): Added a RoomSeeder

---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Room;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Room::create([
+            'name' => Str::random(10),
+            'number' => Str::random(10),
+            'floor' => rand(1, 25),
+            'building' => Str::random(10),
+            'status' => Str::random(5)
+        ]);
+    }
+}
