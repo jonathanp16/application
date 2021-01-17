13cbe244307257ce6e8f2d6b4e705cfe924df7f7
Andrew H <andrew@plankdesign.com>
2021-01-17 15:50:28 -0500

    fix(307): add new vue components from jetstream update

---
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -0,0 +1,76 @@
+<template>
+    <jet-authentication-card>
+        <template #logo>
+            <jet-authentication-card-logo />
+        </template>
+
+        <jet-validation-errors class="mb-4" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <jet-label for="email" value="Email" />
+                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <jet-label for="password" value="Password" />
+                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <jet-label for="password_confirmation" value="Confirm Password" />
+                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Reset Password
+                </jet-button>
+            </div>
+        </form>
+    </jet-authentication-card>
+</template>
+
+<script>
+    import JetAuthenticationCard from '@src/Jetstream/AuthenticationCard'
+    import JetAuthenticationCardLogo from '@src/Jetstream/AuthenticationCardLogo'
+    import JetButton from '@src/Jetstream/Button'
+    import JetInput from '@src/Jetstream/Input'
+    import JetLabel from '@src/Jetstream/Label'
+    import JetValidationErrors from '@src/Jetstream/ValidationErrors'
+
+    export default {
+        components: {
+            JetAuthenticationCard,
+            JetAuthenticationCardLogo,
+            JetButton,
+            JetInput,
+            JetLabel,
+            JetValidationErrors
+        },
+
+        props: {
+            email: String,
+            token: String,
+        },
+
+        data() {
+            return {
+                form: this.$inertia.form({
+                    token: this.token,
+                    email: this.email,
+                    password: '',
+                    password_confirmation: '',
+                })
+            }
+        },
+
+        methods: {
+            submit() {
+                this.form.post(this.route('password.update'), {
+                    onFinish: () => this.form.reset('password', 'password_confirmation'),
+                })
+            }
+        }
+    }
+</script>
