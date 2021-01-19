62eb1a1a1e4b709d06b3aab6b79f5d508f293f37
jonathanp16 <perezjonathan301@gmail.com>
2021-01-18 23:22:59 -0500

    feat(#264): Added room filtering route in api file testing it within the rooms table

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Room;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/filterRooms', function (Request $request) {
+    return Room::where('floor', '>', 5)->get();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
