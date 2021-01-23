b59802cbf58326967fc9bb2ad440d8174155c3df
Evan Dimopoulos <evan.dime@gmail.com>
2021-01-23 14:44:52 -0500

    refactor(#248): Add comments and refactor reused code

--- tests/Feature/RoomRestrictionsTest.php
+++ tests/Feature/RoomRestrictionsTest.php
@@ -13,99 +13,106 @@ class RoomRestrictionsTest extends TestCase
 {
     use RefreshDatabase;
 
-    private $rooms;
-    private $roles;
     private $user;
+    private $rooms;
+    private $test_role1;
+    private $test_role2;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         //Create rooms and roles
-        $rooms = Room::factory()->count(5)->create();
-        $this->rooms = $rooms;
-        $this->roles = Role::factory()->count(5)->create();
+        $this->rooms = Room::factory()->count(5)->create();
+        $roles = Role::factory()->count(5)->create();
 
         //Give test user permission to update rooms.
         Permission::create(['name'=>'bookings.approve', 'guard_name'=> 'web']);
         $this->user = User::factory()->create();
-        $role = $this->roles->first();
+        $role = $roles->first();
         $role->givePermissionTo('bookings.approve');
         $this->user->assignRole($role);
+
+        //Pick our random roles for each test.
+        $test_roles = $roles->random(2);
+        $this->test_role1 = $test_roles->first();
+        $this->test_role2 = $test_roles->last();
     }
 
     /**
-     * Test if the route can create, update and remove restrictions from the system
+     * Test if the route can create restrictions from the system
      *
      * @return void
      */
     public function test_restrictions_controller_create()
     {
-
-        //Pick our random room and roles.
         $test_room = $this->rooms->random();
-        $test_roles = $this->roles->random(2);
-        $test_role1 = $test_roles->first();
-        $test_role2 = $test_roles->last();
-
-        //Test if we can add a restriction if none are there yet.
         $this->assertDatabaseCount('room_restrictions', 0);
+
         $response = $this->actingAs($this->user)->put('room/restrictions/' . $test_room->id, [
-            'restrictions' => [$test_role1->id],
+            'restrictions' => [$this->test_role1->id],
         ]);
         $response->assertStatus(302);
         $this->assertDatabaseCount('room_restrictions', 1);
-        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $test_role1->id]);
+        $this->assertDatabaseHas('room_restrictions',
+            [
+                'room_id' => $test_room->id,
+                'role_id' => $this->test_role1->id
+            ]);
     }
 
+    /**
+     * Test if the route can update number of restrictions from the system
+     *
+     * @return void
+     */
     public function test_restrictions_controller_update_1_to_2()
     {
-        //Pick our random room and roles.
         $test_room = $this->rooms->random();
-        $test_roles = $this->roles->random(2);
-        $test_role1 = $test_roles->first();
-        $test_role2 = $test_roles->last();
-
-        $test_room->restrictions()->sync(collect($test_role1->id));
+        $test_room->restrictions()->sync([$this->test_role1->id]);
+        $this->assertDatabaseCount('room_restrictions', 1);
 
         //Test if we can add another restriction if one is already there
         $response = $this->actingAs($this->user)->put('room/restrictions/' . $test_room->id, [
-            'restrictions' => [$test_role1->id, $test_role2->id],
+            'restrictions' => [$this->test_role1->id, $this->test_role2->id],
         ]);
         $response->assertStatus(302);
         $this->assertDatabaseCount('room_restrictions', 2);
-        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $test_role1->id]);
-        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $test_role2->id]);
+        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $this->test_role1->id]);
+        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $this->test_role2->id]);
     }
 
+    /**
+     * Test if the route can update number of restrictions, by reducing, from the system
+     *
+     * @return void
+     */
     public function test_restrictions_controller_update_2_to_1()
     {
-        //Pick our random room and roles.
         $test_room = $this->rooms->random();
-        $test_roles = $this->roles->random(2);
-        $test_role1 = $test_roles->first();
-        $test_role2 = $test_roles->last();
-
-        $test_room->restrictions()->sync(collect($test_role1->id, $test_role2->id));
+        $test_room->restrictions()->sync([$this->test_role1->id, $this->test_role2->id]);
+        $this->assertDatabaseCount('room_restrictions', 2);
 
         //Test if we can remove a restriction if there are multiple attached and not lose any
         $response = $this->actingAs($this->user)->put('room/restrictions/' . $test_room->id, [
-            'restrictions' => [$test_role2->id],
+            'restrictions' => [$this->test_role2->id],
         ]);
         $response->assertStatus(302);
         $this->assertDatabaseCount('room_restrictions', 1);
-        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $test_role2->id]);
+        $this->assertDatabaseHas('room_restrictions', ['room_id' => $test_room->id, 'role_id' => $this->test_role2->id]);
     }
 
+    /**
+     * Test if the route can remove restrictions from the system
+     *
+     * @return void
+     */
     public function test_restrictions_controller_remove()
     {
-        //Pick our random room and roles.
         $test_room = $this->rooms->random();
-        $test_roles = $this->roles->random(2);
-        $test_role1 = $test_roles->first();
-        $test_role2 = $test_roles->last();
+        $test_room->restrictions()->sync([$this->test_role1->id, $this->test_role2->id]);
+        $this->assertDatabaseCount('room_restrictions', 2);
 
-        $test_room->restrictions()->sync(collect($test_role1->id, $test_role2->id));
         //Test if we can remove restrictions
         $response = $this->actingAs($this->user)->put('room/restrictions/' . $test_room->id, [
             'restrictions' => [],
