39632e199c14547b89f926523518f01e12d669e4
Stojda <alexstojda@outlook.com>
2021-01-25 19:24:00 -0500

    fix: Actually fix logout & add test

--- resources/js/Components/Navbar/Navbar.vue
+++ resources/js/Components/Navbar/Navbar.vue
@@ -45,6 +45,7 @@
                 <jet-dropdown align="right" width="48">
                   <template #trigger>
                     <button
+                      dusk="nav-profile"
                       v-if="$page.jetstream.managesProfilePhotos"
                       class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out"
                     >
@@ -56,6 +57,7 @@
                     </button>
 
                     <button
+                      dusk="nav-profile"
                       v-else
                       class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                     >
@@ -90,8 +92,8 @@
                     <div class="border-t border-gray-100"></div>
 
                     <!-- Authentication -->
-                    <form @submit.prevent="logout">
-                      <jet-dropdown-link as="button"> Logout</jet-dropdown-link>
+                    <form @submit.prevent="logout" >
+                      <jet-dropdown-link as="button" dusk="nav-logout"> Logout</jet-dropdown-link>
                     </form>
                   </template>
                 </jet-dropdown>
