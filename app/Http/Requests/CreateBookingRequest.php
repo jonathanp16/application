c8248ef3f73ee147cbeb84c50eeffb9caed26f8e
Andrew H <andrew@plankdesign.com>
2021-01-27 19:16:09 -0500

    test(#294): fix breaking tests

--- app/Http/Requests/CreateBookingRequest.php
+++ app/Http/Requests/CreateBookingRequest.php
@@ -21,63 +21,12 @@ class CreateBookingRequest extends FormRequest
      */
     public function rules()
     {
-        return [
+        return array_replace_recursive((new UpdateBookingRequest())->rules(), [
             'room_id' => ['required', 'integer'],
 
             'reservations' => ['required'],
             'reservations.*.start_time' => ['required', 'date'],
             'reservations.*.end_time' => ['required', 'date'],
-
-            'onsite_contact.name' => ['required_with:onsite_contact.phone,onsite_contact.email','string', 'max:255'],
-            'onsite_contact.phone' => ['required_with:onsite_contact.name,onsite_contact.email','string'],
-            'onsite_contact.email' => ['required_with:onsite_contact.name,onsite_contact.phone','email'],
-
-            'event.start_time' => [
-                'required',
-                'date_format:H:i',
-                //'after_or_equal:reservations.0.start_time',
-            ],
-            'event.end_time' => [
-                'required',
-                'date_format:H:i',
-                'after:event.start_time',
-                //'before_or_equal:reservations.0.end_time',
-            ],
-            'event.title' => ['required','string', 'max:100'],
-            'event.type' => ['required','string', 'max:50'],
-            'event.description' => ['required','string', 'max:500'],
-            'event.guest_speakers' => ['required','string', 'max:255'],
-            'event.attendees' => ['required','numeric'],
-
-            'event.fee' => ['string', 'max:50'],
-            'event.music' => ['string', 'max:75'],
-            'event.food.low_risk' => ['boolean'],
-            'event.food.high_risk' => ['boolean'],
-            'event.food.self_catered' => ['required_if:event.food.high_risk,true','boolean'],
-            'event.food.caterer' => ['required_if:event.food.self_catered,false','string', 'max:100'],
-            'event.alcohol' => ['boolean'],
-            // remember open states of questions
-            'event.show.contact' => ['boolean'],
-            'event.show.fee' => ['boolean'],
-            'event.show.music' => ['boolean'],
-
-            'event.children' => ['boolean'],
-            'event.appliances' => ['boolean'],
-            'event.av' => ['boolean'],
-            'event.furniture' => ['boolean'],
-            'event.bake_sale' => ['boolean'],
-            'event.internal_meeting' => ['boolean'],
-
-            'notes' => ['nullable','string', 'max:500'],
-            'files' => [
-                'required_if:event.food.high_risk,true',
-                'required_if:event.alcohol,true',
-                'required_if:event.children,true',
-                'required_if:event.appliances,true',
-                'required_if:event.bake_sale,true',
-                //'mimes:pdf,doc,docx',
-                'max:50000', // 50Mb
-            ],
-        ];
+        ]);
     }
 }
