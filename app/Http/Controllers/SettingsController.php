12e0e0738ec9e8f6316c93741faff5f95f946296
Evan Dimopoulos <evan.dime@gmail.com>
2021-02-06 14:56:06 -0500

    task(#448): Using Laravel's build in function to generate a hashed name for files

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Settings;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Response;
 use Inertia\ResponseFactory;
@@ -46,22 +47,22 @@ class SettingsController extends Controller
     }
 
 
-    public function storeAppLogo(Request $request)
-    {
-        $request->validate([
-            'app_logo' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-            'label' => 'required'
-        ]);
-        $path = 'storage/' . Storage::disk('public')->putFileAs(
-            'logos',
-                $request->file('app_logo'),
-                now()->format('d-m-y-H-i-s').'.'.$request->file('app_logo')->extension());
-        Settings::updateOrCreate(
-            ['slug' => $request->label],
-            ['data' => ['path' => $path]]
-        );
-        return back();
-    }
+  public function storeAppLogo(Request $request)
+  {
+    $request->validate([
+      'app_logo' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+      'label' => 'required'
+    ]);
+    $path = '/storage/' . Storage::disk('public')->putFileAs(
+        'logos',
+        $request->file('app_logo'),
+        $request->file('app_logo')->hashName());
+    Settings::updateOrCreate(
+      ['slug' => $request->label],
+      ['data' => ['path' => $path]]
+    );
+    return back();
+  }
 
 
 }
