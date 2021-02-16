3041aa8b008774b18b88a55c0686217ec1f2a190
jonathanp16 <perezjonathan301@gmail.com>
2021-02-16 14:03:02 -0500

    feat(#337): Changed roles ui structure

--- resources/js/Pages/Admin/Roles/CreateRoleForm.vue
+++ resources/js/Pages/Admin/Roles/CreateRoleForm.vue
@@ -1,63 +1,62 @@
 <template>
+    <div>
+        <form-section @submitted="createRole">
+            <template #title>
+                Create a Role
+            </template>
 
-    <!-- Generate Role -->
-    <jet-form-section @submitted="createRole">
-        <template #title>
-            Create Role
-        </template>
 
-        <template #description>
-            Roles are used across the application to authorize users when performing actions or viewing sensitive information.
-        </template>
+            <template #form>
+                <div class="overflow-y-auto max-h-96 w-full pb-3">
+                    
+                    <div class="mb-3 w-4/5">
+                        <jet-label for="name" value="Name"/>
+                        <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus/>
+                        <jet-input-error :message="form.error('name')" class="mt-2"/>
+                    </div>
 
-        <template #form>
-            <!-- Role Name -->
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="name" value="Name"/>
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus/>
-                <jet-input-error :message="form.error('name')" class="mt-2"/>
-            </div>
+                    
+                    <div class="mb-3 w-4/5" v-if="availablePermissions.length > 0">
+                        <jet-label for="permissions" value="Permissions" />
 
-            <!-- Permissions -->
-            <div class="col-span-6" v-if="availablePermissions.length > 0">
-                <jet-label for="permissions" value="Permissions" />
+                        <div class="mt-2 grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div v-for="permission in availablePermissions">
+                                <label class="flex items-center">
+                                    <jet-checkbox :value="permission.name" v-model="form.permissions"/>
+                                    <span class="ml-2 text-md text-black">{{ permission.name }}</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
 
-                <div class="mt-2 grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <div v-for="permission in availablePermissions">
-                        <label class="flex items-center">
-                            <jet-checkbox :value="permission.name" v-model="form.permissions"/>
-                            <span class="ml-2 text-md text-black">{{ permission.name }}</span>
-                            <span class="ml-2 text-sm text-gray-600">{{ permission.guard_name }}</span>
-                        </label>
+                    
+                    <div class="mb-3 w-4/5">
+                        <jet-label for="number_of_bookings_per_period" value="Number of bookings per period (optional)"/>
+                        <jet-input id="number_of_bookings_per_period" type="number" class="mt-1 block w-full"
+                                v-model="form.number_of_bookings_per_period" min="1" autofocus/>
+                        <jet-input-error :message="form.error('number_of_bookings_per_period')" class="mt-2"/>
+                    </div>
+                    <div class="mb-3 w-4/5">
+                        <jet-label for="number_of_days_per_period" value="Number of days per period (optional)"/>
+                        <jet-input id="number_of_days_per_period" type="number" class="mt-1 block w-full"
+                                v-model="form.number_of_days_per_period" min="1" autofocus/>
+                        <jet-input-error :message="form.error('number_of_days_per_period')" class="mt-2"/>
                     </div>
                 </div>
-            </div>
+            </template>
 
-            <!-- Booking restrictions -->
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="number_of_bookings_per_period" value="Number of bookings per period (optional)"/>
-                <jet-input id="number_of_bookings_per_period" type="number" class="mt-1 block w-full"
-                           v-model="form.number_of_bookings_per_period" min="1" autofocus/>
-                <jet-input-error :message="form.error('number_of_bookings_per_period')" class="mt-2"/>
-            </div>
-            <div class="col-span-6 sm:col-span-4">
-                <jet-label for="number_of_days_per_period" value="Number of days per period (optional)"/>
-                <jet-input id="number_of_days_per_period" type="number" class="mt-1 block w-full"
-                           v-model="form.number_of_days_per_period" min="1" autofocus/>
-                <jet-input-error :message="form.error('number_of_days_per_period')" class="mt-2"/>
-            </div>
-        </template>
+            <template #actions>
+                <jet-action-message :on="form.recentlySuccessful" class="mr-3">
+                    Created
+                </jet-action-message>
 
-        <template #actions>
-            <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                Created
-            </jet-action-message>
-
-            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Create
-            </jet-button>
-        </template>
-    </jet-form-section>
+                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Create
+                </jet-button>
+            </template>
+        </form-section>
+    </div>
+    
 </template>
 
 <script>
@@ -68,6 +67,7 @@
     import JetLabel from '@src/Jetstream/Label';
     import JetInput from '@src/Jetstream/Input';
     import JetInputError from '@src/Jetstream/InputError';
+    import FormSection from '@src/Components/FormSection';
 
     export default {
         components: {
@@ -78,6 +78,7 @@
             JetLabel,
             JetInput,
             JetInputError,
+            FormSection
         },
 
         props: {
--- resources/js/Pages/Admin/Roles/Index.vue
+++ resources/js/Pages/Admin/Roles/Index.vue
@@ -1,91 +1,14 @@
 <template>
     <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Roles
-            </h2>
-        </template>
-
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            <create-role-form :availablePermissions="permissions"></create-role-form>
-
-            <div v-if="roles.length > 0">
-                <jet-section-border/>
-
-                <!-- Manage Application Roles -->
-                <div class="mt-10 sm:mt-0">
-                    <jet-action-section>
-                        <template #title>
-                            Manage Roles
-                        </template>
-
-                        <template #description>
-                            You may delete any of your existing roles if they are no longer needed.
-                        </template>
-
-                        <!-- Roles List -->
-                        <template #content>
-                            <div class="space-y-6">
-                                <div v-for="role in roles" class="flex items-center justify-between">
-                                    <div class="flex items-center">
-                                        <div class="text-md mr-3">
-                                            {{ role.name }}
-                                        </div>
-                                        <div class="text-sm text-gray-400">
-                                            {{ role.guard_name }}
-                                        </div>
-                                    </div>
-
-                                    <div class="flex items-center">
-                                        <div v-if="role.created_at" class="text-sm text-gray-400">
-                                            Created {{ $fromNow(role.created_at) }}
-
-                                        </div>
-
-                                        <button class="cursor-pointer ml-6 text-sm text-blue-800 focus:outline-none"
-                                                @click="roleBeingUpdated = role">
-                                            Update
-                                        </button>
-
-                                        <button class="cursor-pointer ml-6 text-sm text-red-500 focus:outline-none"
-                                                @click="roleBeingDeleted = role">
-                                            Delete
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </template>
-                    </jet-action-section>
+        <div class="flex flex-col lg:flex-row md:mx-auto md:w-11/12 ">
+            <div class="m-4 w-1/4 pt-12 pb-48">
+                <create-role-form :availablePermissions="permissions" />
+            </div>
+            <div v-if="roles.length > 0" class="m-4 w-3/4">
+                <div class="py-12">
+                    <RoleTable :roles="roles" :permissions="permissions"/>
                 </div>
             </div>
-
-            <!-- Assign Permissions to Role Modal -->
-            <update-role-form :permissions="permissions"
-                              :role="roleBeingUpdated"
-                              @close="roleBeingUpdated = null"></update-role-form>
-
-            <!-- Delete Role Confirmation Modal -->
-            <jet-confirmation-modal :show="roleBeingDeleted" @close="roleBeingDeleted = null">
-                <template #title>
-                    Delete Role
-                </template>
-
-                <template #content>
-                    Are you sure you would like to delete this role?
-                </template>
-
-                <template #footer>
-                    <jet-secondary-button @click.native="roleBeingDeleted = null">
-                        Nevermind
-                    </jet-secondary-button>
-
-                    <jet-danger-button class="ml-2" @click.native="deleteRole"
-                                       :class="{ 'opacity-25': deleteRoleForm.processing }"
-                                       :disabled="deleteRoleForm.processing">
-                        Delete
-                    </jet-danger-button>
-                </template>
-            </jet-confirmation-modal>
         </div>
     </app-layout>
 </template>
@@ -100,6 +23,8 @@
     import JetButton from '@src/Jetstream/Button'
     import CreateRoleForm from "./CreateRoleForm";
     import UpdateRoleForm from "./UpdateRoleForm";
+    import moment from "moment";
+    import RoleTable from '@src/Components/Tables/RoleTable';
 
     export default {
         props: {
@@ -127,6 +52,7 @@
             JetConfirmationModal,
             CreateRoleForm,
             UpdateRoleForm,
+            RoleTable
         },
 
         data() {
@@ -146,6 +72,11 @@
                     this.roleBeingDeleted = null
                 })
             },
+
+            fromNow(timestamp) {
+                return moment(timestamp).local().fromNow()
+            },
+
         }
     }
 </script>
