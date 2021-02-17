5032f8eb53fb6e21edaddebd9d3e249041092443
Evan Dimopoulos <evan.dime@gmail.com>
2021-02-17 14:11:53 -0500

    fix(#229): move title back to original page to appease teh sonarcloud gods

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+      <title>{{ config('app.name', 'Laravel') }}</title>
       @include('app_head')
     </head>
     <body class="font-sans antialiased">
