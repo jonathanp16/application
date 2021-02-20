347938851f1bd331207a73ef1560aaf9f40d712d
Stojda <alexstojda@outlook.com>
2021-02-20 13:49:28 -0500

    chore: Install hotjar

--- resources/views/app_head.blade.php
+++ resources/views/app_head.blade.php
@@ -16,3 +16,17 @@
 
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}" defer></script>
+
+@if(App::environment('staging'))
+  <!-- Hotjar Tracking Code for http://csu-bp-staging.herokuapp.com/ -->
+  <script>
+    (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:2258124,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  </script>
+@endif
