98870c2987138b869398e68a5a8252a9bdaa8a57
Joey Abou-Chaker <joeyta_ac@hotmail.com>
2021-02-21 19:47:22 -0500

    feat(#333): Added the proper CSU logo and the footer to the login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
+    <x-jet-authentication-card >
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+        <img src="https://www.csu.qc.ca/wp-content/uploads/2020/01/CSU_logo_Colored.png" >
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
@@ -45,4 +45,87 @@
             </div>
         </form>
     </x-jet-authentication-card>
+       <!-- Footer -->
+       <footer class="bg-black text-white text-xs font-bold pb-12">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="flex">
+                    <div class="flex flex-col flex-1">
+                        <div class="flex-1 py-3 font-black text-sm">
+                            CONTACT US
+                        </div>
+                        <div class="flex-1 py-3">
+                            contactus@csu.qc.ca
+                        </div>
+                        <div class="flex-1 py-3">
+                            514 848-7474
+                        </div>
+                    </div>
+                    <div class="flex flex-col flex-1">
+                        <div class="flex-1 py-3 font-black text-sm">
+                            CSU
+                        </div>
+                        <div class="flex-1 py-3">
+                            Our Mission
+                        </div>
+                        <div class="flex-1 py-3">
+                            Contact
+                        </div>
+                        <div class="flex-1 py-3">
+                            Jobs
+                        </div>
+                    </div>
+                    <div class="flex flex-col flex-1">
+                        <div class="flex-1 py-3 font-black text-sm">
+                            SOCIAL MEDIA
+                        </div>
+                        <div class="flex-1 py-3">
+                            <a href="https://www.facebook.com">
+                                <i class="fab fa-facebook pr-2"></i>
+                                Facebook
+                            </a>
+                        </div>
+                        <div class="flex-1 py-3">
+                            <a href="https://www.instagram.com">
+                                <i class="fab fa-instagram pr-2"></i>
+                                Instagram
+                            </a>
+                        </div>
+                        <div class="flex-1 py-3">
+                            <a href="https://www.twitter.com">
+                                <i class="fab fa-twitter pr-2"></i>
+                                Twitter
+                            </a>
+                        </div>
+                        <div class="flex-1 py-3">
+                            <a href="https://www.youtube.com">
+                                <i class="fab fa-youtube pr-2"></i>
+                                YouTube
+                            </a>
+                        </div>
+                    </div>
+                    <div class="flex flex-col flex-1">
+                        <div class="flex-1 py-3 font-black text-sm">
+                            SUBSCRIBE TO OUR NEWSLETTER
+                        </div>
+                      <div class="flex-1 py-3">
+                        <div class="mt-1 relative rounded-sm shadow-sm">
+                          <label for="email">
+                            <input type="text" name="email" id="email"
+                                   class="focus:ring-indigo-500 focus:border-indigo-500 block w-full py-2 pl-1 pr-12 sm:text-sm border-gray-300 rounded-md bg-black border-2 border-white"
+                                   placeholder="Email Address">
+                          </label>
+                        </div>
+                      </div>
+                        <div class="flex-1 py-3">
+                            <div class="mt-1 relative rounded-sm shadow-sm">
+                                <button type="button"
+                                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full py-2 pl-1 pr-12 sm:text-sm border-gray-300 rounded-md bg-white text-black">
+                                    SUBSCRIBE
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
 </x-guest-layout>
