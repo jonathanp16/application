11761a3e16422344c55e1b5611a8c86bc6b2db80
keegan <woods>
2021-02-22 00:38:53 -0500

    fix(#484): final fixes to empty class tags

--- resources/js/Components/Tables/RoleTable.vue
+++ resources/js/Components/Tables/RoleTable.vue
@@ -18,7 +18,7 @@
                 <button
                   class="flex text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                 >
-                  <div class="">Permissions</div>
+                  <div>Permissions</div>
                 </button>
               </template>
 
@@ -36,7 +36,7 @@
                   <button
                   class="flex text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                   >
-                    <div class="">Action</div>
+                    <div>Action</div>
                   </button>
                 </template>
 
