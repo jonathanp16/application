97af061612cc5092ac2b8472ab747c675aa4184c
Alex <ahayeur@gmail.com>
2021-03-14 22:17:57 -0400

    feat(#334): Update Dashboard UI
    Fixed test to accommodate new functionality

--- tests/Javascript/Pages/Dashboard.spec.js
+++ tests/Javascript/Pages/Dashboard.spec.js
@@ -1,14 +1,28 @@
-import Vue from 'vue'
-import { shallowMount } from '@vue/test-utils'
+
 import { InertiaApp } from '@inertiajs/inertia-vue'
-import { InertiaForm } from 'laravel-jetstream'
 import Dashboard from '@src/Pages/Dashboard'
-import {test} from "@jest/globals";
+import {beforeEach, jest, test} from "@jest/globals";
+import {createLocalVue, shallowMount} from '@vue/test-utils'
 
-Vue.use(InertiaApp)
-Vue.use(InertiaForm)
 
-//This is an example, please do more than just test if it crashes :p
+jest.mock('@inertiajs/inertia-vue')
+let localVue
+
+beforeEach(() => {
+  localVue = createLocalVue()
+  localVue.use(InertiaApp)
+});
+
+
 test('should mount without crashing', () => {
-    shallowMount(Dashboard)
+    shallowMount(Dashboard, {localVue,
+        mocks: {
+          $page: {
+            currentRouteName: "dashboard",
+            user: {
+              can: []
+            }
+          }
+        }
+      })
 })
