aadbbca009672e36ac2849d7fb438edf363e896b
Evan Dimopoulos <evan.dime@gmail.com>
2021-03-21 15:17:18 -0400

    test(#535): add room data to bookings test

--- tests/Javascript/Components/Tables/BookingsTable.spec.js
+++ tests/Javascript/Components/Tables/BookingsTable.spec.js
@@ -35,6 +35,7 @@ test('should mount without crashing', () => {
             start_time: "15:00",
             end_time: "15:05",
             status: "available",
+            room: {"id":8, "status": "pending", "name": "CSU Room", "number": "h-619"},
             reservations:  [{"id":1,"room_id":8,"booking_request_id":1,"start_time":"2021-01-26T05:29:15.000000Z","end_time":"2021-01-26T05:31:15.000000Z","created_at":"2021-01-26T05:29:28.000000Z","updated_at":"2021-01-26T05:29:28.000000Z","room":{"id":8,"name":"event3","number":"3","floor":3,"building":"3","created_at":"2021-01-26T05:25:15.000000Z","updated_at":"2021-01-26T05:25:15.000000Z","status":"available","min_days_advance":null,"max_days_advance":null,"attributes":{"food":false,"sofas":null,"chairs":null,"tables":null,"alcohol":false,"computer":false,"projector":false,"fundraiser":false,"television":false,"whiteboard":false,"a_v_permitted":false,"ambiant_music":false,"coffee_tables":null,"sale_for_profit":false,"capacity_sitting":null,"capacity_standing":3},"room_type":"Mezzanine"}}],
             reference:{"path":"9_1611689494_reference"}
         }]
