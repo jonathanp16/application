d837e46f63e058bb7a022bfe27a89ec66245e375
Alec Adub <adub.alec@gmail.com>
2021-04-04 15:50:56 -0400

    test(#347): add dusk test for 347

--- tests/Browser/BookingsSearchTest.php
+++ tests/Browser/BookingsSearchTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\Availability;
 use App\Models\Room;
 use Tests\DuskTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -11,13 +12,38 @@ class BookingsSearchTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
-    public function testUserCanViewRoomsAndAvailabilities()
+    public function testUserCanViewRooms()
     {
         Room::factory()->count(5)->create();
         $this->browse(function (Browser $browser) {
             $browser->loginAs($this->createUserWithPermissions(['bookings.create']));
             $browser->visit('/bookings/search');
             $browser->assertSee(Room::first()->name);
+        });
+    }
+
+    public function testUserCanViewRoomsAvailabilities()
+    {
+        $room = Room::factory()->create();
+        $weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+        foreach ($weekdays as $weekday) {
+            Availability::create([
+                'weekday' => $weekday,
+                'opening_hours' => '07:00',
+                'closing_hours' => '23:00',
+                'room_id' => $room->id
+            ]);
+        }
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($this->createUserWithPermissions(['bookings.create']))
+                ->visit('/bookings/search')
+                ->press('#create')
+                ->waitForText('Availabilities')
+                ->assertSee('07:00')
+                ->assertSee('23:00')
+                ->assertVisible('#availabilities-calendar');
         });
     }
 }
