751e5b7cdbec7081adb1d01bfccb2a1b662b381f
Alec Adub <adub.alec@gmail.com>
2021-04-04 14:14:33 -0400

    test(#247): Add dusk test for 247

--- tests/Browser/RoomsPageTest.php
+++ tests/Browser/RoomsPageTest.php
@@ -44,6 +44,7 @@ class RoomsPageTest extends DuskTestCase
       'status' => $room->status, 'room_type' => $room->room_type
     ]);
   }
+
   public function testWhenUpdateNewRoomWithValidInformation()
   {
     (new RolesAndPermissionsSeeder())->run();
@@ -67,6 +68,7 @@ class RoomsPageTest extends DuskTestCase
       'name' => 'Not Ipsum',
     ]);
   }
+
   public function testWhenDeleteRoom()
   {
     (new RolesAndPermissionsSeeder())->run();
@@ -88,32 +90,57 @@ class RoomsPageTest extends DuskTestCase
     $this->assertDatabaseCount('rooms', 0);
   }
 
-    public function testWhenUpdateRoomRestrictions()
-    {
-        (new RolesAndPermissionsSeeder())->run();
+  public function testWhenUpdateRoomRestrictions()
+  {
+    (new RolesAndPermissionsSeeder())->run();
 
-        $room = Room::factory()->create();
-        $admin = User::factory()->create();
-        $admin->assignRole('super-admin');
-        $role = Role::where('name', 'super-admin')->first();
-        $this->browse(function (Browser $browser) use ($room, $admin, $role) {
-
-
-            $browser->loginAs($admin);
-            $browser->visit(new Rooms)
-                ->assertSee($room->name)
-                ->restrictRoom($room, $role->id);
-            $browser->waitUntilMissingText('NEVERMIND');
-        });
-
-        $this->assertDatabaseCount('room_restrictions', 1);
-        $this->assertDatabaseHas('room_restrictions', [
-            'role_id' => $role->id,
-            'room_id' => $room->id
-        ]);
-    }
+    $room = Room::factory()->create();
+    $admin = User::factory()->create();
+    $admin->assignRole('super-admin');
+    $role = Role::where('name', 'super-admin')->first();
+    $this->browse(function (Browser $browser) use ($room, $admin, $role) {
 
 
+      $browser->loginAs($admin);
+      $browser->visit(new Rooms)
+        ->assertSee($room->name)
+        ->restrictRoom($room, $role->id);
+      $browser->waitUntilMissingText('NEVERMIND');
+    });
+
+    $this->assertDatabaseCount('room_restrictions', 1);
+    $this->assertDatabaseHas('room_restrictions', [
+      'role_id' => $role->id,
+      'room_id' => $room->id
+    ]);
+  }
+
+  public function testUpdateRoomAvailabilitiesWithValidInformation()
+  {
+    (new RolesAndPermissionsSeeder())->run();
+
+    $room = Room::factory()->create();
+    $this->browse(function (Browser $browser) use ($room) {
+
+      $admin = User::factory()->create();
+      $admin->assignRole('super-admin');
+      $browser->loginAs($admin);
+      $browser->visit(new Rooms)
+        ->assertSee($room->name)
+        ->press('Action')
+        ->press('Update')
+        ->type('#Monday_opening_hours', '07')
+        ->type('#Monday_opening_hours', '00')
+        ->type('#Monday_opening_hours', 'AM')
+        ->type('#Monday_closing_hours', '08')
+        ->type('#Monday_closing_hours', '00')
+        ->type('#Monday_closing_hours', 'AM')
+        ->press('#updateRoom');
+      $browser->waitUntilMissingText('NEVERMIND');
+    });
+
+    $this->assertDatabaseCount('availabilities', 1);
+  }
 
     public function testWhenUpdateRoomDateRestrictions()
     {
