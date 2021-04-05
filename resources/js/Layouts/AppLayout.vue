fded50cbef2544dea023bdaa004e600f128d2d7e
Alex <ahayeur@gmail.com>
2021-04-04 21:41:10 -0400

    feat(#597): Removed Newsletter subscribe form

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -19,7 +19,7 @@
         </portal-target>
 
         <!-- Footer -->
-        <footer class="bg-black text-white text-xs font-bold pb-12">
+        <footer class="bg-black text-white text-sm font-bold pb-12">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <div class="flex">
                     <div class="flex flex-col flex-1">
@@ -74,28 +74,6 @@
                                 <i class="fab fa-youtube pr-2"></i>
                                 YouTube
                             </a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col flex-1">
-                        <div class="flex-1 py-3 font-black text-sm">
-                            SUBSCRIBE TO OUR NEWSLETTER
-                        </div>
-                      <div class="flex-1 py-3">
-                        <div class="mt-1 relative rounded-sm shadow-sm">
-                          <label for="email">
-                            <input type="text" name="email" id="email"
-                                   class="focus:ring-indigo-500 focus:border-indigo-500 block w-full py-2 pl-1 pr-12 sm:text-sm border-gray-300 rounded-md bg-black border-2 border-white"
-                                   placeholder="Email Address">
-                          </label>
-                        </div>
-                      </div>
-                        <div class="flex-1 py-3">
-                            <div class="mt-1 relative rounded-sm shadow-sm">
-                                <button type="button"
-                                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full py-2 pl-1 pr-12 sm:text-sm border-gray-300 rounded-md bg-white text-black">
-                                    SUBSCRIBE
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </div>
