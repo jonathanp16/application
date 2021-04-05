02117e3a3120f8bf335b6b42835c44d793dbda01
Joey Abou-Chaker <joeyta_ac@hotmail.com>
2021-04-04 21:15:13 -0400

    fix(#22): Added dusk tests for #22 and #18

--- tests/Browser/SearchPageTest.php
+++ tests/Browser/SearchPageTest.php
@@ -22,7 +22,7 @@ class SearchPageTest extends DuskTestCase
     private array $weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 
-    public function testWhenBookRoomDateRestrictions()
+    public function testWhenBookRoomCustomDateRestrictions()
     {
         (new RolesAndPermissionsSeeder())->run();
         $room = Room::factory()->create();
@@ -78,6 +78,143 @@ class SearchPageTest extends DuskTestCase
 
             $browser->click("#createBookingRequest")->pause(5000)
                 ->assertSee("sooner than 31 days");
+        });
+    }
+
+    public function testWhenBookRoomDateFail()
+    {
+        (new RolesAndPermissionsSeeder())->run();
+        $room = Room::factory()->create(["min_days_advance"=> 2, "max_days_advance"  => 5]);
+        $admin = User::factory()->create();
+        $admin->assignRole('super-admin');
+        $role = Role::where('name', 'super-admin')->first();
+    
+        foreach ($this->weekdays as $weekday) {
+            Availability::create([
+                'weekday' => $weekday,
+                'opening_hours' => '07:00',
+                'closing_hours' => '23:00',
+                'room_id' => $room->id
+            ]);
+        }
+        $this->browse(function (Browser $browser) use ($room, $admin) {
+            $browser->loginAs($admin);
+
+
+            $startId = 'start_time_0';
+            $endId = 'end_time_0';
+            $browser->visit('/bookings/search')
+                ->assertSee($room->name)
+                ->click('@room-select-' . $room->id)
+                ->mouseover('@createBookingModal')
+                ->mouseover('#addAnotherDate');
+
+            $browser->within( new DateTimePicker($startId), function($browser) {
+                $browser->setDatetime(1,13);
+            })->pause(1000);
+
+            $browser->within( new DateTimePicker($endId), function($browser) {
+                $browser->setDatetime(1,14);
+            })->pause(1000);
+
+            $browser->click("#createBookingRequest")->pause(5000)
+              ->assertSee("later than 2 days");
+
+            $browser->within( new DateTimePicker($startId), function($browser) {
+                $browser->setDatetime(10,13);
+            })->pause(1000);
+
+            $browser->within( new DateTimePicker($endId), function($browser) {
+                $browser->setDatetime(10,14);
+            })->pause(1000);
+
+            $browser->click("#createBookingRequest")->pause(5000)
+              ->assertSee("sooner than 5 days");
+
+        });
+    }
+
+        public function testWhenBookRoomTimefail()
+    {
+        (new RolesAndPermissionsSeeder())->run();
+        $room = Room::factory()->create(["min_days_advance"=> 0, "max_days_advance"  => 5]);
+        $admin = User::factory()->create();
+        $admin->assignRole('super-admin');
+        $role = Role::where('name', 'super-admin')->first();
+    
+        foreach ($this->weekdays as $weekday) {
+            Availability::create([
+                'weekday' => $weekday,
+                'opening_hours' => '07:00',
+                'closing_hours' => '23:00',
+                'room_id' => $room->id
+            ]);
+        }
+        $this->browse(function (Browser $browser) use ($room, $admin) {
+            $browser->loginAs($admin);
+
+
+            $startId = 'start_time_0';
+            $endId = 'end_time_0';
+            $browser->visit('/bookings/search')
+                ->assertSee($room->name)
+                ->click('@room-select-' . $room->id)
+                ->mouseover('@createBookingModal')
+                ->mouseover('#addAnotherDate');
+
+            $browser->within( new DateTimePicker($startId), function($browser) {
+                $browser->setDatetime(1,1);
+            })->pause(1000);
+
+            $browser->within( new DateTimePicker($endId), function($browser) {
+                $browser->setDatetime(1,2);
+            })->pause(1000);
+
+            $browser->click("#createBookingRequest")->pause(5000)
+              ->assertSee("These dates and times are not within the room's availabilities!");
+
+        });
+    }
+
+    public function testWhenBookRoomSuccess()
+    {
+        (new RolesAndPermissionsSeeder())->run();
+        $room = Room::factory()->create(["min_days_advance"=> 2, "max_days_advance"  => 5]);
+        $admin = User::factory()->create();
+        $admin->assignRole('super-admin');
+        $role = Role::where('name', 'super-admin')->first();
+    
+        foreach ($this->weekdays as $weekday) {
+            Availability::create([
+                'weekday' => $weekday,
+                'opening_hours' => '07:00',
+                'closing_hours' => '23:00',
+                'room_id' => $room->id
+            ]);
+        }
+        $this->browse(function (Browser $browser) use ($room, $admin) {
+            $browser->loginAs($admin);
+
+
+            $startId = 'start_time_0';
+            $endId = 'end_time_0';
+            $browser->visit('/bookings/search')
+                ->assertSee($room->name)
+                ->click('@room-select-' . $room->id)
+                ->mouseover('@createBookingModal')
+                ->mouseover('#addAnotherDate');
+
+            $browser->within( new DateTimePicker($startId), function($browser) {
+                $browser->setDatetime(3,13);
+            })->pause(1000);
+
+            $browser->within( new DateTimePicker($endId), function($browser) {
+                $browser->setDatetime(3,14);
+            })->pause(1000);
+
+            $browser->click("#createBookingRequest")->pause(5000)
+                ->assertpathis("/bookings/create");
+
         });
     }
 
