9fd5f0d0608e2ab54223bc76f866358e098daf91
Stojda <alexstojda@outlook.com>
2021-04-05 01:54:43 -0400

    fix(#602): Improve UI for Create Room form

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,85 +1,85 @@
 <template>
-    <div class="min-h-screen flex flex-col justify-between">
-        <Navbar/>
+  <div class="min-h-screen flex flex-col justify-between">
+    <Navbar/>
 
-        <!-- Page Heading -->
-        <header class="bg-white shadow" v-if="!!this.$slots[ 'header' ] || !!this.$scopedSlots[ 'header' ]">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <slot name="header"></slot>
+    <!-- Page Heading -->
+    <header class="bg-white shadow" v-if="!!this.$slots[ 'header' ] || !!this.$scopedSlots[ 'header' ]">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <slot name="header"></slot>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <main class="flex-grow">
+      <slot></slot>
+    </main>
+
+    <!-- Footer -->
+    <footer class="bg-black text-white text-sm font-bold pb-12">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="flex">
+          <div class="flex flex-col flex-1">
+            <div class="flex-1 py-3 font-black text-sm">
+              CONTACT US
             </div>
-        </header>
-
-        <!-- Page Content -->
-        <main class="flex-grow">
-            <slot></slot>
-        </main>
-
-        <!-- Modal Portal -->
-        <portal-target name="modal" multiple>
-        </portal-target>
-
-        <!-- Footer -->
-        <footer class="bg-black text-white text-sm font-bold pb-12">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <div class="flex">
-                    <div class="flex flex-col flex-1">
-                        <div class="flex-1 py-3 font-black text-sm">
-                            CONTACT US
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="mailto:contactus@csu.qc.ca">contactus@csu.qc.ca</a>
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="tel:5148487474">514 848-7474</a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col flex-1">
-                        <div class="flex-1 py-3 font-black text-sm">
-                            CSU
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="https://www.csu.qc.ca/about-us/our-mission/">Our Mission</a>
-                        </div>
-                        <div class="flex-1 py-3">
-                           <a href="https://www.csu.qc.ca/about-us/contact/">Contact</a>
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="https://www.csu.qc.ca/about-us/jobs/">Jobs</a>
-                        </div>
-                    </div>
-                    <div class="flex flex-col flex-1">
-                        <div class="flex-1 py-3 font-black text-sm">
-                            SOCIAL MEDIA
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="https://www.facebook.com/csumtl/">
-                                <em class="fab fa-facebook pr-2"></em>
-                                Facebook
-                            </a>
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="https://www.instagram.com/csumtl/">
-                                <em class="fab fa-instagram pr-2"></em>
-                                Instagram
-                            </a>
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="https://twitter.com/csumtl">
-                                <em class="fab fa-twitter pr-2"></em>
-                                Twitter
-                            </a>
-                        </div>
-                        <div class="flex-1 py-3">
-                            <a href="https://www.youtube.com/channel/UC14BOYjvdSqaCoCJfede9SA">
-                                <em class="fab fa-youtube pr-2"></em>
-                                YouTube
-                            </a>
-                        </div>
-                    </div>
-                </div>
+            <div class="flex-1 py-3">
+              <a href="mailto:contactus@csu.qc.ca">contactus@csu.qc.ca</a>
             </div>
-        </footer>
-    </div>
+            <div class="flex-1 py-3">
+              <a href="tel:5148487474">514 848-7474</a>
+            </div>
+          </div>
+          <div class="flex flex-col flex-1">
+            <div class="flex-1 py-3 font-black text-sm">
+              CSU
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://www.csu.qc.ca/about-us/our-mission/">Our Mission</a>
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://www.csu.qc.ca/about-us/contact/">Contact</a>
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://www.csu.qc.ca/about-us/jobs/">Jobs</a>
+            </div>
+          </div>
+          <div class="flex flex-col flex-1">
+            <div class="flex-1 py-3 font-black text-sm">
+              SOCIAL MEDIA
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://www.facebook.com/csumtl/">
+                <em class="fab fa-facebook pr-2"></em>
+                Facebook
+              </a>
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://www.instagram.com/csumtl/">
+                <em class="fab fa-instagram pr-2"></em>
+                Instagram
+              </a>
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://twitter.com/csumtl">
+                <em class="fab fa-twitter pr-2"></em>
+                Twitter
+              </a>
+            </div>
+            <div class="flex-1 py-3">
+              <a href="https://www.youtube.com/channel/UC14BOYjvdSqaCoCJfede9SA">
+                <em class="fab fa-youtube pr-2"></em>
+                YouTube
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Modal Portal -->
+    <portal-target name="modal" multiple>
+    </portal-target>
+  </div>
 </template>
 
 <script>
@@ -92,25 +92,25 @@ import JetResponsiveNavLink from '@src/Jetstream/ResponsiveNavLink'
 import Navbar from '@src/Components/Navbar/Navbar'
 
 export default {
-    components: {
-        JetApplicationLogo,
-        JetApplicationMark,
-        JetDropdown,
-        JetDropdownLink,
-        JetNavLink,
-        JetResponsiveNavLink,
-        Navbar
-    },
+  components: {
+    JetApplicationLogo,
+    JetApplicationMark,
+    JetDropdown,
+    JetDropdownLink,
+    JetNavLink,
+    JetResponsiveNavLink,
+    Navbar
+  },
 
-    data() {
-        return {
-            showingNavigationDropdown: false,
-        }
-    },
-    computed: {
-        path() {
-            return window.location.pathname
-        }
+  data() {
+    return {
+      showingNavigationDropdown: false,
     }
+  },
+  computed: {
+    path() {
+      return window.location.pathname
+    }
+  }
 }
 </script>
--- resources/js/Pages/Admin/Rooms/Index.vue
+++ resources/js/Pages/Admin/Rooms/Index.vue
@@ -1,18 +1,16 @@
 <template>
-    <app-layout>
-        <div class="flex flex-col lg:flex-row md:mx-auto md:w-11/12">
-            <div class="m-4 w-1/4 pt-12 pb-48">
-                <create-room-form :available-room-types="availableRoomTypes"
-                :available-buildings="availableBuildings"/>
-            </div>
-            <div v-if="rooms.length > 0" class="m-4 w-3/4">
-                <div class="mx-24 mt-16">
-                    <ViewActiveRoomsTable :rooms="rooms" :available-room-types="availableRoomTypes"
-                                          :available-buildings="availableBuildings" :roles="roles"/>
-                </div>
-            </div>
-        </div>
-    </app-layout>
+  <app-layout>
+    <div class="mt-12 flex flex-col items-center lg:flex-row mx-10">
+      <div class="m-4 p-2">
+        <create-room-form :available-room-types="availableRoomTypes"
+                          :available-buildings="availableBuildings"/>
+      </div>
+      <div v-if="rooms.length > 0" class="px-3 ">
+        <ViewActiveRoomsTable :rooms="rooms" :available-room-types="availableRoomTypes"
+                              :available-buildings="availableBuildings" :roles="roles"/>
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
@@ -22,33 +20,33 @@ import AppLayout from '@src/Layouts/AppLayout';
 import ViewActiveRoomsTable from '@src/Components/Tables/ViewActiveRoomsTable';
 
 export default {
-    components: {
-        CreateRoomForm,
-        AppLayout,
-        JetSectionBorder,
-        ViewActiveRoomsTable,
+  components: {
+    CreateRoomForm,
+    AppLayout,
+    JetSectionBorder,
+    ViewActiveRoomsTable,
+  },
+  props: {
+    rooms: {
+      type: Array,
+      default: function () {
+        return []
+      },
     },
-    props: {
-        rooms: {
-            type: Array,
-            default: function () {
-                return []
-            },
-        },
-        roles: {
-            type: Array,
-            default: function () {
-                return []
-            },
-        },
-        availableRoomTypes: {
-            type: Array,
-            required: true
-        },
-        availableBuildings: {
-          type: Array,
-          required: true
-        },
-    }
+    roles: {
+      type: Array,
+      default: function () {
+        return []
+      },
+    },
+    availableRoomTypes: {
+      type: Array,
+      required: true
+    },
+    availableBuildings: {
+      type: Array,
+      required: true
+    },
+  }
 }
 </script>
