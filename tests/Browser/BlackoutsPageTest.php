623f24fd0a6ef038c9ada6cb4441dee62117cbba
Andrew H <andrew@plankdesign.com>
2021-04-06 22:59:06 -0400

    test(#581): improve create blackout dusk test

--- tests/Browser/BlackoutsPageTest.php
+++ tests/Browser/BlackoutsPageTest.php
@@ -10,6 +10,7 @@ use Database\Seeders\EasyUserSeeder;
 use Database\Seeders\RolesAndPermissionsSeeder;
 use Facebook\WebDriver\WebDriverKeys;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Str;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Components\DateTimePicker;
 use Tests\DuskTestCase;
@@ -30,7 +31,7 @@ class BlackoutsPageTest extends DuskTestCase
     }
     public function testCanCreateBlackouts()
     {
-        Room::factory()->create();
+        $room = Room::factory()->create();
 
         AcademicDate::create([
             'semester' => 'Fall',
@@ -38,25 +39,31 @@ class BlackoutsPageTest extends DuskTestCase
             'end_date' => Carbon::now()->addDays(5)
         ]);
 
-        $this->browse(function (Browser $browser) {
-            $browser->loginAs(User::first())->visit('/admin/rooms/1/blackouts');
+        $this->browse(function (Browser $browser) use ($room) {
+            $browser->loginAs(User::first())->visitRoute('admin.rooms.blackouts.index', $room);
 
-            $browser->within(new DateTimePicker("start"), function ($browser) {
-                $browser->setDatetime(15, 7);
+            $name = Str::random();
+            $start = today()->addDays(15)->setHour(7);
+            $end = today()->addDays(15)->setHour(8);
+
+            $browser->within(new DateTimePicker("start"), function ($browser) use ($start) {
+                $browser->setDatetime(15, $start->hour);
             })->pause(1000);
 
-            $browser->within(new DateTimePicker("end"), function ($browser) {
-                $browser->setDatetime(15, 8);
+            $browser->within(new DateTimePicker("end"), function ($browser) use ($end) {
+                $browser->setDatetime(15, $end->hour);
             })->pause(1000);
 
-            $browser->type('#name', 'alex')
-                ->press('#submit')
-                ->pause(8000);
+            $browser->type('#name', $name)->press('#submit');
 
-                $browser->assertSee('alex');
+            $browser->waitUntilVue('blackouts[0].name', $name, '@blackout-list');
+            $browser->assertVue('blackouts[0].start_time', $start->toJSON(), '@blackout-list');
+            $browser->assertVue('blackouts[0].end_time', $end->toJSON(), '@blackout-list');
+
+            $browser->assertSeeIn('@blackout-list', $start->isoFormat('LLL'));
+            $browser->assertSeeIn('@blackout-list', $end->isoFormat('LLL'));
+
         });
-
-        
     }
 
     public function testCanCreateRecuringBlackouts()
