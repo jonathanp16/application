a8368ffcd03e5c460b1e6ec4cc506adb446e4cb4
Alex <ahayeur@gmail.com>
2021-04-10 19:52:45 -0400

    feat(#629): branded 503 error page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1><img src="img/default_app_logo.png"></h1>
+    <div>
+        <p>The booking service is temporarily unavailable. We'll be back soon! If this page persists, please contact us at <a href="mailto:it@csu.qc.ca">it@csu.qc.ca</a>. Otherwise we&rsquo;ll be back shortly!</p>
+        <p>&mdash; Concordia Student Union</p>
+    </div>
+</article>
