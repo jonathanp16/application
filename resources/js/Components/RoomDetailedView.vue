cfe5b19a6a4f6118b3927b65fe7b0b49e27de923
Alec Adub <adub.alec@gmail.com>
2021-04-11 22:30:22 -0400

    fix(#658): remove unecessary room details

--- resources/js/Components/RoomDetailedView.vue
+++ resources/js/Components/RoomDetailedView.vue
@@ -12,18 +12,17 @@
           <hr/>
         </div>
         <div class="flex flex-wrap grid grid-cols-3 gap-4" v-if="room">
-          <div v-for="(attribute, key) in room.attributes" :key="attribute.id">
-            <jet-label :for="convertVarToText(key)" :value="convertVarToText(key)+':'"></jet-label>
-            <div v-if="typeof(attribute) == 'boolean'">
-              <div v-if="attribute" class="text-center rounded-full py-2 px-4 bg-green-300">Yes</div>
-              <div v-else class="text-center rounded-full py-2 px-4 bg-red-400">No</div>
-            </div>
-            <div v-if="typeof(attribute) == 'number' || Number.isInteger(attribute)">
-              <div class="text-center rounded-full py-2 px-4 bg-gray-300">{{attribute}}</div>
-            </div>
-            <div v-if="typeof(attribute) == 'string'">
-              <div class="text-center rounded-full py-2 px-4 bg-gray-300">{{attribute}}</div>
-            </div>
+          <div v-for="(attribute, key) in room.attributes" :key="attribute.id" v-if="(typeof(attribute) !== 'boolean') || attribute">
+              <jet-label :for="convertVarToText(key)" :value="convertVarToText(key)+':'"></jet-label>
+              <div v-if="typeof(attribute) == 'boolean'">
+                <div v-if="attribute" class="text-center rounded-full py-2 px-4 bg-green-300">Yes</div>
+              </div>
+              <div v-if="typeof(attribute) == 'number' || Number.isInteger(attribute)">
+                <div class="text-center rounded-full py-2 px-4 bg-gray-300">{{attribute}}</div>
+              </div>
+              <div v-if="typeof(attribute) == 'string'">
+                <div class="text-center rounded-full py-2 px-4 bg-gray-300">{{attribute}}</div>
+              </div>
           </div>
           <div>
             <jet-label :for="'room_type'" :value="'Room Type:'"></jet-label>
